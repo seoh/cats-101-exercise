d04ecf26f4f5cfb97adc1fa055cf6f1ade4d8172
seoh <devthewild@gmail.com>
2017-06-26 00:39:08 +0900

    migration ch03 to sbt

---
+++ ch03/Contamap1.scala
@@ -0,0 +1,29 @@
+/**
+  * 3.6.1.1
+  */
+object Contamap1 extends App {
+
+  trait Printable[A] {
+    def format(value: A): String
+
+    def contramap[B](func: B => A): Printable[B] = {
+      val self = this
+      new Printable[B] {
+        def format(value: B): String =
+          self.format(func(value))
+      }
+    }
+  }
+
+  def format[A](value: A)(implicit p: Printable[A]): String =
+    p.format(value)
+
+  implicit val printableInt = new Printable[Int] {
+    def format(value: Int): String = value.toString
+  }
+
+  implicit val printableDouble =
+    printableInt.contramap((d: Double) => d.toInt)
+
+  println(format(1.5))
+}
